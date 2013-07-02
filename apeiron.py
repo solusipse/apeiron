d755552236fcc6c5aacabcf2c4d5d215725a0fca
solusipse <solus1ps3@gmail.com>
2013-07-02 10:32:20 +0200

    minor fix

--- apeiron.py
+++ apeiron.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, session, redirect, url_for, escape, re
 import generator as Generator
 import json, sys
 
-app = Flask('apeiron')
+app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
